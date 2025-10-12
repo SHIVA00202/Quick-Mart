dc8133c017db1332ec545676096f7138ab15711c
SHIVA00202 <shivayadav1238800@gmail.com>
2025-10-12 15:37:41 +0530

    order generating

--- backend/routes/orderroutes.js
+++ backend/routes/orderroutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import isAuth from "../middlewares/isAuth.js"
-import { getMyOrders, placeOrder, updateOrderStatus } from "../controllers/ordercontroller.js"
+import { getDeliveryBoyAssignment, getMyOrders, placeOrder, updateOrderStatus } from "../controllers/ordercontroller.js"
 
 
 
@@ -11,6 +11,7 @@ const orderRouter=express.Router()
 orderRouter.post("/place-order",isAuth,placeOrder)
 orderRouter.get("/my-orders",isAuth,getMyOrders)
 orderRouter.post("/update-status/:orderId/:shopId",isAuth,updateOrderStatus)
+orderRouter.get("/get-assignments",isAuth,getDeliveryBoyAssignment)
 
 
 export default orderRouter
