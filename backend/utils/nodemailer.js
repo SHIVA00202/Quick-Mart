819d3668539672d5291267b628f804d4cd546638
SHIVA00202 <shivayadav1238800@gmail.com>
2025-10-21 08:43:22 +0530

    Update nodemailer.js

--- backend/utils/nodemailer.js
+++ backend/utils/nodemailer.js
@@ -3,23 +3,28 @@ import nodemailer from "nodemailer"
 import dotenv from "dotenv"
 dotenv.config()
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  port: 465,
-  secure: true, // true for 465, false for other ports
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false, // TLS
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASS,
   },
 });
 
-export const sendOtpMail=async (to,otp) => {
+export const sendOtpMail = async (to, otp) => {
+  try {
     await transporter.sendMail({
-        from:process.env.EMAIL,
-        to,
-        subject:"Reset Your Password",
-        html:`<p>Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.</p>`
-    })
-}
+      from: process.env.EMAIL,
+      to,
+      subject: "Reset Your Password",
+      html: `<p>Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.</p>`,
+    });
+    console.log("✅ OTP sent to", to);
+  } catch (err) {
+    console.error("❌ send otp error", err);
+  }
+};
 
 export const sendDeliveryOtpMail =async (user,otp) => {
     await transporter.sendMail({
