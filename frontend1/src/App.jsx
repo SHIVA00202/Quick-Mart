c655b146bde5fc683cb903c99e0dcb68b20cf524
SHIVA00202 <shivayadav1238800@gmail.com>
2025-10-22 09:06:46 +0530

    Update App.jsx

--- frontend1/src/App.jsx
+++ frontend1/src/App.jsx
@@ -25,7 +25,7 @@ import { io } from 'socket.io-client'
 import { setSocket } from './redux/userSlice'
 
 
-export const serverUrl="https://quick-mart-1ailbackend.onrender.com"
+export const serverUrl="https://quick-mart-1bbbbbbbbbb.onrender.com"
 const App = () => {
   const dispatch=useDispatch()
   useGetCurrentUser()
