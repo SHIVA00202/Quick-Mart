c51b4a44187dec9cab4181dad2a04de28b410268
SHIVA00202 <shivayadav1238800@gmail.com>
2025-10-22 10:20:07 +0530

    Update nodemailer.js

--- backend/utils/nodemailer.js
+++ backend/utils/nodemailer.js
@@ -1,31 +1,52 @@
 import nodemailer from "nodemailer"
+import { createTransport } from "nodemailer";
+
 
 import dotenv from "dotenv"
 dotenv.config()
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  port: 465,
-  secure: true, // true for 465, false for other ports
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASS,
-  },
-});
+const sendOtpMail = async (to, otp) => {
+  const transport = createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+  });
+  const html = `<p>Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.</p>`;
+  await transport.sendMail({
+    from: process.env.EMAIL,
+    to,
+    subject:"Reset Your Password",
+    html,
+  });
+};
+export default sendOtpMail;
 
-export const sendOtpMail=async (to,otp) => {
-    await transporter.sendMail({
-        from:process.env.EMAIL,
-        to,
-        subject:"Reset Your Password",
-        html:`<p>Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.</p>`
-    })
+
+ const sendDeliveryOtpMail =async (user,otp) => {
+   const transport = createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+  });
+   html:`<p>Your OTP for delivery is <b>${otp}</b>. It expires in 5 minutes.</p>`
+  await transport.sendMail({
+    from: process.env.EMAIL,
+    to:user.email,
+    subject:"Your Delivery OTP ",
+    html,
+  });
+};
+export default sendDeliveryOtpMail;
+
+  
+  
+  
 }
 
-export const sendDeliveryOtpMail =async (user,otp) => {
-    await transporter.sendMail({
-        from:process.env.EMAIL,
-        to:user.email,
-        subject:"Your Delivery OTP",
-        html:`<p>Your OTP for delivery is <b>${otp}</b>. It expires in 5 minutes.</p>`
-    })
-}
+
+
