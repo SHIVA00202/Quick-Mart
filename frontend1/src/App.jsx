d07a76569d1b2c8834191951bad9db9e98bd319a
Shiva <shivayadav1238800@gmail.com>
2025-10-27 02:47:46 +0530

    Update App.jsx

--- frontend1/src/App.jsx
+++ frontend1/src/App.jsx
@@ -25,7 +25,7 @@ import { io } from 'socket.io-client'
 import { setSocket } from './redux/userSlice'
 
 
-export const serverUrl="https://quick-mart-l8gx.vercel.app"
+export const serverUrl="https://quick-mart-2-rlt3.onrender.com"
 console.log("hello ")
 const App = () => {
   const dispatch=useDispatch()
