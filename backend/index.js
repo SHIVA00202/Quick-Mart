5c72d4d0c045ef2c9327f5cf9a94393c377b8a83
SHIVA00202 <shivayadav1238800@gmail.com>
2025-10-26 23:25:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const app=express()
 const server=http.createServer(app)
 const io=new Server(server,{
     cors:{
-        origin:"https://quick-mart-2vqc.vercel.app/signin",
+        origin:"https://quick-mart-2vqc.vercel.app",
         credentials:true,
         methods:['POST','GET']
     }
@@ -28,7 +28,7 @@ app.set("io",io)
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"https://quick-mart-2vqc.vercel.app/signin",
+    origin:"https://quick-mart-2vqc.vercel.app",
     credentials:true
      
 
