cc7a43bf081b85f66145365b7e7f9801b1ac054e
Shiva <shivayadav1238800@gmail.com>
2025-11-08 19:38:36 +0530

    Update UserDashboard.jsx

--- frontend1/src/components/UserDashboard.jsx
+++ frontend1/src/components/UserDashboard.jsx
@@ -96,7 +96,7 @@ const UserDashboard = () => {
       {searchItems && searchItems.length > 0 && (
         <div className='w-full max-w-6xl flex flex-col gap-5 items-start p-5 bg-white shadow-md rounded-2xl mt-4'>
           <h1 className='text-gray-900 text-2xl sm:text-3xl font-semibold border-b border-gray-200 pb-2'>
-            Search Results
+            Search Resultss
           </h1>
           <div className='w-full h-auto flex flex-wrap gap-6 justify-center'>
             {searchItems.map(item => <FoodCard data={item} key={item._id} />)}
